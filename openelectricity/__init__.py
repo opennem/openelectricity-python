692f8cf0c468af7c742b7465f855f63687541f94
Nik Cubrilovic <git@nikcub.me>
2025-08-19 10:12:53 +1000

    feat: OE styles module and examples

--- openelectricity/__init__.py
+++ openelectricity/__init__.py
@@ -11,3 +11,10 @@ __name__ = "openelectricity"
 __version__ = "0.5.1"
 
 __all__ = ["OEClient", "AsyncOEClient"]
+
+# Optional imports for styling (won't fail if dependencies are missing)
+try:
+    from openelectricity import styles
+    __all__.append("styles")
+except ImportError:
+    pass  # Styling module requires matplotlib/seaborn which are optional
