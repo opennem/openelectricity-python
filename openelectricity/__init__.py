1d66171d174cb2861dfe3836e7ceb9486cfa0a56
Nik Cubrilovic <git@nikcub.me>
2025-08-20 09:19:20 +1000

    Bump version to 0.5.2

--- openelectricity/__init__.py
+++ openelectricity/__init__.py
@@ -8,7 +8,7 @@ from openelectricity.client import AsyncOEClient, OEClient
 
 __name__ = "openelectricity"
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 __all__ = ["OEClient", "AsyncOEClient"]
 
